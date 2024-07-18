d49be3cb2b288acfefc72bf59ff3181f69063a0e
Sypth <105406568+Sypth@users.noreply.github.com>
2024-07-18 17:54:44 +0800

    :100: Finalizing designs for Forms, Pages, and Flash Messages. Pagination is also implemented in Index

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::get('/', function () {
 });
 
 Route::get('/tasks', function () {
-    $tasks = Task::latest()->where('completed', true)->get();
+    $tasks = Task::latest()->paginate(10);
 
     return view('index', [
         'tasks' => $tasks
@@ -69,6 +69,11 @@ Route::delete('/tasks/{task}', function (Task $task) {
     return redirect()->route('tasks.index')
         ->with('success', 'Task deleted successfully');
 })->name('tasks.destroy');
+
+Route::put('tasks/{task}/toggle-complete', function (Task $task) {
+    $task->toggleComplete();
+    return redirect()->back()->with('success', 'Task completed successfully');
+})->name('tasks.toggle-complete');
 
 Route::fallback(function () {
     return 'Still got somewhere!';
